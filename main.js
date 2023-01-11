11b9ad8399abc6810e17121ce10a52509b0c1215
Carlos Valencia <valenciacarlos278@gmail.com>
2023-01-10 21:49:44 -0500

    Mobile burger menu and shopping cart interactions added

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burgerMenuIcon = document.querySelector(".menu");
+const menuCartIcon = document.querySelector(".navbar-shopping-cart");
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-
+burgerMenuIcon.addEventListener('click',toggleMobileMenu);
+menuCartIcon.addEventListener('click',toggleCartAside);
 
 function toggleDesktopMenu(){
-    
-    desktopMenu.classList.toggle("inactive")
 
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive')    
+    }
+
+    desktopMenu.classList.toggle("inactive")
 }
+
+function toggleMobileMenu(){
+    
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive')    
+    }
+
+    mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCartAside(){{
+
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive')    
+    }
+
+    aside.classList.toggle('inactive')
+    
+}}
